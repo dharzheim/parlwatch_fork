8165fb9969f0f304e80970a1e65e312e0442bc59
dharzheim <danharzheim@gmail.com>
2025-03-25 19:54:23 +0100

    feat(i18n): check if german text has changed, if yes update all other languages

--- translate.ts
+++ translate.ts
@@ -1,6 +1,9 @@
 const fs = require('node:fs');
 
 const { Translate } = require('@google-cloud/translate').v2;
+const simpleGit = require('simple-git');
+
+const git = simpleGit.default();
 
 const texts = {
   de: require('./src/assets/i18n/de.json'),
@@ -22,6 +25,8 @@ const targetLanguages = Object.keys(texts).filter(
 const replaceAll = process.argv.includes('--replace-all');
 const withoutCapitalization = process.argv.includes('--without-capitalization');
 
+translateTexts();
+
 /**
  *
  */
@@ -29,19 +34,23 @@ async function translateTexts() {
   if (!process.env.CLOUD_TRANSLATE_API_KEY) {
     console.log('No API key defined. Please define CLOUD_TRANSLATE_API_KEY');
   }
+
   console.log('Translating texts...');
   const originalTexts = texts[originalLanguage];
+
+  const updatedKeys = await getUpdatedKeys(originalTexts);
 
   for (const key in originalTexts) {
     const text = originalTexts[key];
 
-    const translationsNeeded = replaceAll
-      ? targetLanguages
-      : targetLanguages.filter(
-          (lang) =>
-            texts[lang][key] === undefined ||
-            texts[lang][key].startsWith('Missing value')
-        );
+    const translationsNeeded =
+      replaceAll || updatedKeys.includes(key)
+        ? targetLanguages
+        : targetLanguages.filter(
+            (lang) =>
+              texts[lang][key] === undefined ||
+              texts[lang][key].startsWith('Missing value')
+          );
     const translatedTexts = await translateText(text, translationsNeeded);
 
     console.log(`Translated "${key}"`);
@@ -62,6 +71,34 @@ async function translateTexts() {
   }
 }
 
+const mapVariablse = (text: string): { [key: string]: string } => {
+  const variables = text.match(/{{\s*[\w.]+\s*}}/g);
+  if (!variables) {
+    return {};
+  }
+  return variables.reduce((mappedVariables, variable, index) => {
+    return { ...mappedVariables, [variable]: `{{${index}}}` };
+  }, {});
+};
+
+const replaceVariables = (
+  text: string,
+  mappedVariables: { [key: string]: string }
+) => {
+  return Object.keys(mappedVariables).reduce((acc, key) => {
+    return acc.replaceAll(key, mappedVariables[key]);
+  }, text);
+};
+
+const reverseReplaceVariables = (
+  text: string,
+  mappedVariables: { [key: string]: string }
+) => {
+  return Object.keys(mappedVariables).reduce((acc, key) => {
+    return acc.replaceAll(mappedVariables[key], key);
+  }, text);
+};
+
 /**
  *
  * @param text Text to translate
@@ -72,15 +109,15 @@ async function translateText(
   text: string,
   targetLanguages: string[]
 ): Promise<Partial<{ [key in Language]: string }>> {
-  const variables = map_variables(text);
-  const textWithReplacedVariables = replace_variables(text, variables);
+  const variables = mapVariablse(text);
+  const textWithReplacedVariables = replaceVariables(text, variables);
   const translations = {};
   for (const targetLanguage of targetLanguages) {
     const [translation] = await translate.translate(textWithReplacedVariables, {
       from: originalLanguage,
       to: targetLanguage
     });
-    translations[targetLanguage] = reverse_replace_variables(
+    translations[targetLanguage] = reverseReplaceVariables(
       translation,
       variables
     );
@@ -88,32 +125,37 @@ async function translateText(
   return translations;
 }
 
-const map_variables = (text: string): { [key: string]: string } => {
-  const variables = text.match(/{{\s*[\w.]+\s*}}/g);
-  if (!variables) {
-    return {};
+/**
+ * @returns Current texts in original language on main branch
+ */
+async function getCurrentOriginTextsFromMain(): Promise<{
+  [key: string]: string;
+} | null> {
+  try {
+    const fileContent = await git.show(
+      `main:src/assets/i18n/${originalLanguage}.json`
+    );
+    const originalTextsFromMain = JSON.parse(fileContent);
+    console.log('Original texts:', originalTextsFromMain);
+    return originalTextsFromMain;
+  } catch (error) {
+    console.error('Error getting current origin texts from main');
+    return null;
   }
-  return variables.reduce((mappedVariables, variable, index) => {
-    return { ...mappedVariables, [variable]: `{{${index}}}` };
-  }, {});
-};
+}
 
-const replace_variables = (
-  text: string,
-  mappedVariables: { [key: string]: string }
-) => {
-  return Object.keys(mappedVariables).reduce((acc, key) => {
-    return acc.replaceAll(key, mappedVariables[key]);
-  }, text);
-};
-
-const reverse_replace_variables = (
-  text: string,
-  mappedVariables: { [key: string]: string }
-) => {
-  return Object.keys(mappedVariables).reduce((acc, key) => {
-    return acc.replaceAll(mappedVariables[key], key);
-  }, text);
-};
-
-translateTexts();
+/**
+ * @param currentOriginalTexts Object with keys and texts in original language
+ * @returns Array of keys that have changed compared to current state on main branch
+ */
+async function getUpdatedKeys(currentOriginalTexts: {
+  [key: string]: string;
+}): Promise<string[]> {
+  const originalTextsFromMain = await getCurrentOriginTextsFromMain();
+  if (!originalTextsFromMain) {
+    return Object.keys(currentOriginalTexts);
+  }
+  return Object.keys(currentOriginalTexts).filter(
+    (key) => currentOriginalTexts[key] !== originalTextsFromMain[key]
+  );
+}
